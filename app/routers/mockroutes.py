6c97edce29862fb547702ac69fe474d682ca33aa
alkihuri <alkihuri@gmail.com>
2025-03-21 07:41:27 +0100

    mock routes upd

--- app/routers/mockroutes.py
+++ app/routers/mockroutes.py
@@ -6,33 +6,22 @@
 from fastapi import APIRouter, HTTPException
 
 router = APIRouter()
+ 
 
-@router.get("/mock/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+# mock touyters for mock duels for queez games between two players
 
-# register a new user
-@router.post("/mock/register")
-async def register_user(user: dict):
-    return user
+@router.get("/mockduel/{player1}/{player2}")
+async def mockduel(player1: str, player2: str):
+    return {"player1": player1, "player2": player2, "winner": player1}
 
-# login a user
-@router.post("/mock/login")
-async def login_user(user: dict):
-    return user
+@router.get("/mockduel/{player1}/{player2}/{winner}")
+async def mockduel(player1: str, player2: str, winner: str):
+    return {"player1": player1, "player2": player2, "winner": winner}
 
-# get all users
+@router.get("/mockduel/{player1}/{player2}/{winner}/{loser}")
+async def mockduel(player1: str, player2: str, winner: str, loser: str):
+    return {"player1": player1, "player2": player2, "winner": winner, "loser": loser}
 
-@router.get("/mock/users")
-async def get_users():
-    return {"users": "users"}
-
-# get user by id
-@router.get("/mock/user/{user_id}") 
-async def get_user(user_id: int):
-    return {"user_id": user_id}
-
-# update user by id
-@router.put("/mock/user/{user_id}")
-async def update_user(user_id: int, user: dict):
-    return {"user_id": user_id, "user": user}
+@router.get("/mockduel/{player1}/{player2}/{winner}/{loser}/{score}")
+async def mockduel(player1: str, player2: str, winner: str, loser: str, score: int):
+    return {"player1": player1, "player2": player2, "winner": winner, "loser": loser, "score": score}
